907e790a2365233ed026554d91ad10747f15d792
Shubhgithu123 <shubhamwangekar2003@gmail.com>
2025-12-27 10:24:06 +0530

    Add study projects and assets

---
+++ JS script/index.js
@@ -0,0 +1,76 @@
+// //operators
+// let a =5;
+
+// // console.log(a++)
+// // console.log(++a)
+// // console.log(a)
+
+
+// console.log(a)
+// console.log(--a)
+
+//assignment operators
+
+// let a =5 ; 
+// // a=a+10 > a+=10
+// a+=10;
+
+// console.log(a)
+// console.log(a)
+ 
+
+//logical operators AND && OR|| NOT!
+
+//falsy and truthy
+
+//Bitwise operator
+
+//and
+
+console.log(2&5) //0
+
+// or
+
+console.log(2|5) //7
+
+//not 
+
+console.log(~0)
+
+// left-shift <<
+
+console.log(5<<4)
+
+let day = 6;
+day = day.toString();
+  switch(day){
+    case "1":
+                console.log("Monday")
+                break;
+    case "2":
+                console.log("Tuesday")
+                break;
+
+    case "3":
+                console.log("Wednesday")
+                break;
+
+    case "4":
+                console.log("Thursday")
+                break;
+
+    case "5":
+                console.log("Friday")
+                break;
+
+    case "6":
+                console.log("Saturday")
+                break;
+
+    case "7":
+                console.log("Sunday")
+                break;
+
+    default : 
+                console.log("Invalid input")
+  }
